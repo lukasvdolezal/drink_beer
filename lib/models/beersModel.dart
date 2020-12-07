8fefbef9b945848e3ca72743d50d49748af773b9
Lukáš Doležal <lukasdolezal@Lukas-MacBook-Pro.local>
2020-12-07 12:22:12 +0100

    adding comments to some fuctions

--- lib/models/beersModel.dart
+++ lib/models/beersModel.dart
@@ -43,15 +43,23 @@ class BeerModel extends ChangeNotifier {
   }
 
   void updateBoozeH(double input) {
+    // Updates average voltage of beers within boozehound
+
     bh.avgVoltage = ((bh.avgVoltage * bh.beerCount) + input) / (++bh.beerCount);
   }
 
   void updateList(Beer beer) {
+    // Adds new beer to list and sorts it alphabetically
+
     toView.add(beer);
     toView.sort((a, b) => a.name.compareTo(b.name));
   }
 
   void add(Beer beer) {
+    // Adds chosen beer to myBeers, create empty opinion
+    // Updates list for ListView
+    // Updates voltage in boozehound
+
     myBeers[beer.name] = beer;
     myOps[beer.name] = Opinion(id: beer.id, rating: 0, comment: '');
     updateList(beer);
